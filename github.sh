d191e7f9f6498073f77046eabbd04e473f043e44
raigomes <rai.gomes.ismart@gmail.com>
2016-12-17 02:22:16 -0200

    [4.9] Incluindo paineis flutuantes com os produtos no corpo do site

--- github.sh
+++ github.sh
@@ -6,8 +6,12 @@ cd /home/rai/Documentos/Curso\ Web\ -\ Caelum/HTML\,\ CSS\ e\ JavaScript/
 git init
 git add *
 read -p "Commit description: " desc  
-git commit -m "$desc"
-git pull origin
-git status
-git push -f origin master
-
+if [$desc == ''] 
+then
+	echo "Empty description!"
+else
+	git commit -m "$desc"
+	git pull origin
+	git status
+	git push -f origin master
+fi
