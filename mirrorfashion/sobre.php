25230e0058628d1d68f96f56748d334b7c3fdae0
raigomes <rai.gomes.ismart@gmail.com>
2017-01-15 21:22:24 -0200

    Primeiro teste com php: Alterando a página Sobre para incluir uma instrução Date em php 7

--- mirrorfashion/sobre.php
+++ mirrorfashion/sobre.php
@@ -34,7 +34,7 @@
 			<h1 class="titulo">A Mirror Fashion</h1>
 
 			<p>A <strong>Mirror Fashion</strong> é a maior empresa comércio eletrônico no segmento de moda em todo o mundo. 
-				<em>Fundada em 1932, possui filiais em 124 países, sendo líder de mercado com mais de 90% de participação em 118 deles.</em></p>
+				<em>Fundada há <?php print date("Y") - 1932; ?> anos, possui filiais em 124 países, sendo líder de mercado com mais de 90% de participação em 118 deles.</em></p>
 
 				<p>Nosso centro de distribuição fica em <a href="https://maps.google.com.br/?q=Jacarezinho, Paraná">Jacarezinho, no Paraná</a>. De lá, saem 48 aviões que distribuem nossos produtos às casas do mundo todo. Nosso centro de distribuição:</p>
 
